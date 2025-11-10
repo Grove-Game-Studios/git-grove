838326bb915951cc495a456d98ef5c0fc3090e2c
Ollie <oliver2.thomson@live.uwe.ac.uk>
2025-11-10 04:34:03 +0000

    Dont want to do it this way

--- Scripts/RepoView.gd
+++ Scripts/RepoView.gd
@@ -2,17 +2,27 @@ extends Control
 class_name RepoView
 
 @onready var repo_path: Label = $VBoxContainer/RepoPath
-@onready var list_of_commits: VBoxContainer = $VBoxContainer/HBoxContainer/Panel2/VBoxContainer/ScrollContainer/ListOfCommits
+@onready var commit_history: Tree = $VBoxContainer/HBoxContainer/Panel2/VBoxContainer/CommitHistory
+
+var root
 
 func set_repo_path(path: String):
 	repo_path.text = path
 	
-	var repo_log = get_log()
+	var repo_log := get_log()
 	print(repo_log)
-	for commit in repo_log:
-		var new_label = Label.new()
-		new_label.text = commit
-		list_of_commits.add_child(new_label)
+	
+	commit_history.set_column_title(0, "Branch")
+	commit_history.set_column_title(1, "Graph")
+	commit_history.set_column_title(2, "Commit Message")
+	
+	root = commit_history.create_item()
+	
+	var commits = repo_log[0].split("\n\n")
+	for commit in commits:
+		var row := commit_history.create_item(root)
+		row.set_text(0, commit)
+		
 
 func get_status() -> Array[String]:
 	var output = GitService.execute_command(repo_path.text, "status", [])

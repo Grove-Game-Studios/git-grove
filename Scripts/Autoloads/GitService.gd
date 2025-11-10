3b71d0631291a6ac4269a23d21e1f2db822fa7ab
Ollie <oliver2.thomson@live.uwe.ac.uk>
2025-11-10 03:19:37 +0000

    Can create a repo

--- Scripts/Autoloads/GitService.gd
+++ Scripts/Autoloads/GitService.gd
@@ -2,3 +2,21 @@ extends Node
 
 func _ready() -> void:
 	print("GitService running")
+
+func execute_command(path: String, command: String, flags: Array[String]) -> bool:
+	var args = []
+
+	args.append("-C")
+	args.append(path)
+	args.append(command)
+	
+	for flag in flags:
+		args.append(flag)
+		
+	var output = []
+	
+	OS.execute("git", args, output)
+	
+	print(output)
+	
+	return true
---
+++ Scripts/CreateRepo.gd
@@ -0,0 +1,48 @@
+extends Control
+
+@onready var path: LineEdit = $VBoxContainer/HBoxContainer/Path
+@onready var file_dialog: FileDialog = $VBoxContainer/HBoxContainer/FileDialog
+@onready var project_name: LineEdit = $VBoxContainer/ProjectName
+@onready var message: Label = $VBoxContainer/Message
+
+func _ready() -> void:
+	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
+	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_DIR
+
+func _on_browse_button_button_down() -> void:
+	file_dialog.popup_centered_ratio(0.8)
+
+
+func _on_file_dialog_dir_selected(dir: String) -> void:
+	path.text = dir
+
+func _on_create_button_button_down() -> void:
+	if project_name.text == "":
+		message.text = "Project must have a name"
+		return
+		
+	var dir = folder_is_empty()
+	if dir != "":
+		if create_repo(dir):
+			message.text = "Created repo"
+		else:
+			message.text = "Could not create repo"
+		
+func folder_is_empty() -> String:
+	var parent_dir = DirAccess.open(path.text)
+	
+	if parent_dir == null:
+		message.text = "Parent folder does not exist"
+		return ""
+	
+	if parent_dir.dir_exists(project_name.text):
+		message.text = "Project folder already exists"
+		return ""
+	
+	parent_dir.make_dir(project_name.text)
+	parent_dir.change_dir(project_name.text)
+		
+	return parent_dir.get_current_dir()
+	
+func create_repo(repo_path: String) -> bool:
+	return GitService.execute_command(repo_path, "init", [])

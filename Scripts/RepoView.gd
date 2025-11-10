17facb93a031b6312bd3e7ff54e2d9ae4f2d6ace
Ollie <oliver2.thomson@live.uwe.ac.uk>
2025-11-10 03:42:55 +0000

    Rough layout to repo view

--- Scripts/RepoView.gd
+++ Scripts/RepoView.gd
@@ -1,7 +1,7 @@
 extends Control
 class_name RepoView
 
-@onready var repo_path: Label = $RepoPath
+@onready var repo_path: Label = $VBoxContainer/RepoPath
 
 func set_repo_path(path: String):
 	repo_path.text = path
